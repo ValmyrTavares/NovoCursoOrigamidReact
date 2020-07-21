f924ffc1bc15fd832d8f6da4efc3600a0743f97a
Valmyr Tavares <valmyr.lima@millennium.com.br>
2020-07-21 11:58:05 -0300

    Esse é um exemplo de componentes sendo usados como na vida real, ou que mais se aproxima disso, Pequenos componentes como titulos são feitos e reutilizados em outros componentes. E tem um exemplo execente de map dentro dos componentes que eu registrei na pasta de React pela relevancia. E temos aqui um exemplo de Style in line que eu tomei divessos paus para fazer sem sucesso

--- src/App.js
+++ src/App.js
@@ -1,54 +1,26 @@
 import React, { Fragment } from 'react'
-
-const Luana = {
-    cliente: 'Mario',
-    idade: 27,
-    compras:[
-        {nome:"Nonebook", preco: 'R$ 2500'},
-        {nome:"Geladeira", preco: 'R$ 3000'},
-        {nome:"Smartphone", preco: 'R$ 1500'},       
-    ],
-    ativa: true
-}
+import Home from './desafio/Home'
+import Produtos from './desafio/Produtos'
+import Header from './desafio/Header'
 
 
-const Mario = {
-    cliente: 'Mario',
-    idade: 31,
-    compras:[
-        {nome:"Nonebook", preco: 'R$ 2500'},
-        {nome:"Geladeira", preco: 'R$ 3000'},
-        {nome:"Smartphone", preco: 'R$ 1500'},
-        {nome:"Guitarra", preco: 'R$ 3500'},
-    ],
-    ativa: false
-}
+
 
 const App = () => {
-  
-  const dados = Mario;
-  const compras = dados.compras
-  .map((item)=>  Number( item.preco.replace("R$", "")))
-  .reduce((a, b)=> a + b)
-     
-
-
-      function soma(){
-        compras.replace((x, y)=>{
-            return x + y
-        })
-    }
-    console.log(compras)
-    
+  const {pathname} = window.location;
+  let Pagina;
+  if(pathname==='/')
+ { Pagina = Home
+}else{
+  Pagina = Produtos
+}
 
   return (
       <Fragment>
-        <p>Nome: {dados.cliente}</p>
-        <p>Idade: {dados.idade} anos</p>
-        <p>Situação: <span style={{color: dados.ativa ? 'green': 'red'}}> {dados.ativa===true ?  "Ativa": "Inativa"}</span></p>
-        <p>Total de gastos {compras}</p>
-        <p>{compras>10000 && "Vc está gastnado demais"}</p>
-
+        <Header/>
+        <Pagina/>
+      
+      
       </Fragment>
   )
 }
---
+++ src/Title.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Title = ({texto})=>{
+    return(
+        <h1 style={{color:'green'}}>
+            {texto}
+        </h1>
+    )
+}
+export default Title;
