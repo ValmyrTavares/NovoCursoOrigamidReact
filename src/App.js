f5d3c75a914a71a509c6d271791fa397416e88f8
Valmyr Tavares <valmyr.lima@millennium.com.br>
2020-07-21 16:19:49 -0300

    Aula 302 use Efect 1 É o principio de useEffect da forma mais facil que existe renderizando um botão de clique que soma mais um ao resultado. Um bom exemplo pra começar agora ele vai usar o fetch para puxar um api no momento que o componente é criado com um array vazio

--- src/App.js
+++ src/App.js
@@ -1,33 +1,19 @@
 import React, { Fragment } from 'react'
-import Produto from './produto'
+
 
 
 const App = ()=>{
+  const [contar, setContar] = React.useState(0)
 
-  const botao = {
-    color:'red',
-    padding:'15px 20px',
-    background:'blue',
-    margin:'5rem',
-    borderRadius:'5px'
-  }
-  const [dados, setDados] = React.useState(null)
-  const [carregando, setCarregando] = React.useState(null)
-
-  async function handleClick(event){
-    setCarregando(true)
-   const response = await fetch(`https:ranekapi.origamid.dev/json/api/produto/${event.target.innerText}`)
-   const json = await response.json();
-   setDados(json)
-   setCarregando(false)
-  }
+  React.useEffect (() =>{
+    console.log("Executou")
+  },[contar])
+ 
   return (
     <Fragment>
-      <button style={botao}onClick={handleClick}>NoteBook</button>
-      <button style={botao}onClick={handleClick}>Smartphone</button>
-      <button style={botao}onClick={handleClick}>Tablet</button>
-      {carregando && <p>Carregando</p>}
-     {!carregando && dados && <Produto dados={dados}/>}
+      <button onClick={()=>setContar(contar + 1)}>Clique aqui</button>
+      <p>{contar}</p>
+   
     </Fragment>
   )
 }
