894aed428c062b0897cf8a37ceb174ba02fe510c
Valmyr Tavares <valmyr.lima@millennium.com.br>
2020-07-20 22:48:00 -0300

    Esse é um pequeno exercício de javascript com react que é ouro, Eu dou o destaque para acondicional de style dentro do span. Eu sabia fazer mas fiquei nervoso

--- src/App.js
+++ src/App.js
@@ -1,34 +1,55 @@
 import React, { Fragment } from 'react'
 
-const App = () => {
-    const carro = {
-        marca:"Ford",
-        rodas: 4
-    }
+const Luana = {
+    cliente: 'Mario',
+    idade: 27,
+    compras:[
+        {nome:"Nonebook", preco: 'R$ 2500'},
+        {nome:"Geladeira", preco: 'R$ 3000'},
+        {nome:"Smartphone", preco: 'R$ 1500'},       
+    ],
+    ativa: true
+}
 
-    const estiloP = {
-        color:"blue",
-        border: "solid 1px black",
-        fontSize: '30px'
+
+const Mario = {
+    cliente: 'Mario',
+    idade: 31,
+    compras:[
+        {nome:"Nonebook", preco: 'R$ 2500'},
+        {nome:"Geladeira", preco: 'R$ 3000'},
+        {nome:"Smartphone", preco: 'R$ 1500'},
+        {nome:"Guitarra", preco: 'R$ 3500'},
+    ],
+    ativa: false
+}
+
+const App = () => {
+  
+  const dados = Mario;
+  const compras = dados.compras
+  .map((item)=>  Number( item.preco.replace("R$", "")))
+  .reduce((a, b)=> a + b)
+     
+
+
+      function soma(){
+        compras.replace((x, y)=>{
+            return x + y
+        })
     }
-    const nome = "Andre"
-    const titulo = <h1>Issso é um titulo</h1>
-    function mostraNome(sobrenome){
-        return "Valmyr" + sobrenome
-    }
-    return (
-        <Fragment>
-    <a href="https://www.origamid.com" title="Isso é um site">
-        Origamid
-        </a>
-        <input type="text" id="nome"/>
-        <p>{nome}</p>
-        {titulo}
-        {mostraNome("Lima")}
-    <p>{new Date().getFullYear()}</p>
-    <p style={estiloP}>{carro.marca}</p>
-    <p>{carro.rodas}</p>
-        </Fragment>
-        )
+    console.log(compras)
+    
+
+  return (
+      <Fragment>
+        <p>Nome: {dados.cliente}</p>
+        <p>Idade: {dados.idade} anos</p>
+        <p>Situação: <span style={{color: dados.ativa ? 'green': 'red'}}> {dados.ativa===true ?  "Ativa": "Inativa"}</span></p>
+        <p>Total de gastos {compras}</p>
+        <p>{compras>10000 && "Vc está gastnado demais"}</p>
+
+      </Fragment>
+  )
 }
 export default App;
