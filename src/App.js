4d1b173df1ddc0bbcd732b62c2ecf0320925afd1
Valmyr Tavares <valmyr.lima@millennium.com.br>
2020-07-21 15:56:40 -0300

    Aula 301 state 3. É a parte do desafio, que é criar um app com 3 botões que traga o nome preço e imagem de um api. Tudo fácil mas que denota de prática. Esse sempre é o problema, tudo precisa de muita prática, aponto de eu achar que não sei nada, não me sinto realmente seguro pra fazer nada. Claro que eu faço, mas queria fazer as coisas mais rápidas, com mais soluções. Enfim. Com certeza esse será um commit bastante acessado

--- src/App.js
+++ src/App.js
@@ -1,17 +1,36 @@
 import React, { Fragment } from 'react'
-import ButtonModal from './ButtonModal'
-import Modal from './Modal'
-
-const App = () => {
- const [modal, setModal] = React.useState(false)
+import Produto from './produto'
 
 
+const App = ()=>{
+
+  const botao = {
+    color:'red',
+    padding:'15px 20px',
+    background:'blue',
+    margin:'5rem',
+    borderRadius:'5px'
+  }
+  const [dados, setDados] = React.useState(null)
+  const [carregando, setCarregando] = React.useState(null)
+
+  async function handleClick(event){
+    setCarregando(true)
+   const response = await fetch(`https:ranekapi.origamid.dev/json/api/produto/${event.target.innerText}`)
+   const json = await response.json();
+   setDados(json)
+   setCarregando(false)
+  }
   return (
-    <Fragment>      
-      <div>{modal ? "Modal Aberto": " Modal Fechado"}</div>
-        <ButtonModal setModal={setModal} />
-        <Modal modal={modal} setModal={setModal} />
+    <Fragment>
+      <button style={botao}onClick={handleClick}>NoteBook</button>
+      <button style={botao}onClick={handleClick}>Smartphone</button>
+      <button style={botao}onClick={handleClick}>Tablet</button>
+      {carregando && <p>Carregando</p>}
+     {!carregando && dados && <Produto dados={dados}/>}
     </Fragment>
   )
 }
+
 export default App;
+
