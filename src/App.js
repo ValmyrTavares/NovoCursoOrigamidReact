7dd2200b456203eb3cc7fcc895c8dcb7eff6fda0
Valmyr Tavares <valmyr.lima@millennium.com.br>
2020-07-21 12:05:46 -0300

    COmo eu vou deletar o disable gostaria de registrar o uso dele Estou na aula 301- React hOOKS

--- src/App.js
+++ src/App.js
@@ -1,26 +1,15 @@
 import React, { Fragment } from 'react'
-import Home from './desafio/Home'
-import Produtos from './desafio/Produtos'
-import Header from './desafio/Header'
+
 
 
 
 
 const App = () => {
-  const {pathname} = window.location;
-  let Pagina;
-  if(pathname==='/')
- { Pagina = Home
-}else{
-  Pagina = Produtos
-}
+ const ativo = false;
 
   return (
-      <Fragment>
-        <Header/>
-        <Pagina/>
-      
-      
+      <Fragment>      
+        <button disabled={!ativo}>{ativo ? 'Ativo': 'Inativo'}</button>
       </Fragment>
   )
 }
