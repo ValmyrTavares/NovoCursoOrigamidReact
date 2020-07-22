f2edc4c14805eaded552e7b7739226c25d8c0049
Valmyr Tavares <valmyr.lima@millennium.com.br>
2020-07-22 16:11:29 -0300

    Essa é uma introdução a useContext que faz com que todos os componentes se comuniquem. Parte importante Esse é

--- src/App.js
+++ src/App.js
@@ -1,38 +1,17 @@
 import React from 'react'
-import Product from './Product'
+import UserContext from './UserContext'
+import Produto from './Produto'
 
 
 
 const App = ()=>{
-  const [produto, setProduto] = React.useState(null)
-  const stilo = {
-    background:"green",
-    padding:'15px 25px',
-    color:'white',
-    margin: '10px'
-  }
 
-  React.useEffect(()=>{
-    const produtoLocal = window.localStorage.getItem('produto')
-    if (produtoLocal!==null) setProduto(produtoLocal)
-  },[])
-
-  React.useEffect(()=>{
-   if(produto !==null) window.localStorage.setItem('produto', produto)
-  },[produto])
-
-  function handleClick(event){
-    setProduto(event.target.innerText)
-  }
- 
- 
   return (
-   <React.Fragment>
-     <p>Esse é o {produto}</p>
-     <button onClick={handleClick} style={stilo}>Smartphone</button>
-     <button onClick={handleClick} style={stilo}>Notebook</button>
-     <Product produto={produto} />
-   </React.Fragment>
+  
+     <UserContext.Provider value={{value: "Andre"}}>
+      <Produto />
+     </UserContext.Provider>    
+ 
   )
   }
 
