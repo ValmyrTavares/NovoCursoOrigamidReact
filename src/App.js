e3d3476e42e4de2f41f7f13fb807636abb80859f
Valmyr Tavares <valmyr.lima@millennium.com.br>
2020-07-23 12:32:06 -0300

    Uma textarea com o forma velha de se alterar no react

--- src/App.js
+++ src/App.js
@@ -1,94 +1,27 @@
 import React from 'react'
 
-const formFields = [
-  {id:'nome',
-   label:'Nome',
-   type: 'text' 
-},
-{id:'email',
-label:'Email',
-type: 'email' 
-},
-{id:'senha',
-label:'Senha',
-type: 'password' 
-},
-{id:'cep',
-label:'CEP',
-type: 'text' 
-},
-{id:'rua',
-label:'Rua',
-type: 'text' 
-},
-{id:'numero',
-label:'Numero',
-type: 'text' 
-},
-{id:'bairro',
-label:'Bairro',
-type: 'text' 
-},
-{id:'cidade',
-label:'Cidade',
-type: 'text' 
-},
-{id:'estado',
-label:'Estado',
-type: 'text' 
-}
-]
+
 
 
 const App = ()=>{
-  const [form, setForm] = React.useState({
-    nome: '',
-    email:'',
-    senha: "",
-    cep:"",
-    rua:"",
-    numero:"",
-    bairro:"",
-    cidade:"",
-    estado:""
-  })
+  
 
-  const [response, setResponse]= React.useState(null)
+  const [mensagem, setMensagem]= React.useState('')
 
-  function handleChange({target}){
-    const {id, value} = target
-   setForm({...form, [id]: value})
-    console.log(id, value)
-  }
+  
 
-  function handleSubmit(event){
-    event.preventDefault();
-    fetch('https://ranekapi.origamid.dev/json/api/usuario',{
-      method: 'POST',
-      headers:{
-        'Content-Type':'application/json'
-      },
-      body: JSON.stringify(form),
-    }).then(response => {
-      setResponse(response)
-    })
-  }
+  
   
     return (    
-     
-        <form  onSubmit={handleSubmit}>
-          {formFields.map(({id, label, type}) => (
-            <div key={id}>
-              <label htmlFor={id}>{label}</label>
-              <input id={id} name="nome" type={type} value={form[id]} onChange={handleChange}/>
-            </div>
-          ))}
-          {response && response.ok && <p>Formulario Enviado com sucesso</p>}
-            <button>Enviar</button>       
-          
+      <div>
+        <form  >
+         <textarea type="text" value={mensagem} onChange={({target}) => setMensagem(target.value) }/>
         </form>
+        <p>{mensagem}</p>
+      </div>
+);
+
       
-    );
  
 }
 
