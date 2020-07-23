972e169ad6845052eec2e72e5248de4a687679c2
Valmyr Tavares <valmyr.lima@millennium.com.br>
2020-07-22 22:09:02 -0300

    Esse hook custom é muito interessante mas no final a aula ficou com muitos detalhes e eu acabei denxando pra depois, excesso de detalhes que eu ainda nao soeu capaz de digerir

--- src/App.js
+++ src/App.js
@@ -1,17 +1,27 @@
 import React from 'react'
-import Produto from './Produto'
-import { GlobalStorage } from './GlobalContext'
-import Limpar from './Limpar'
+import useFetch from './useFetch'
+
 
 
 const App = ()=>{
 
-  return (    
-    <GlobalStorage>
-      <Produto />   
-      <Limpar />
-    </GlobalStorage>
-  )
+  const {request, data, loading, error} = useFetch();
+
+  React.useEffect(()=> {
+    request('https://ranekapi.origamid.dev/json/api/produto')
+  }, [])
+
+
+  if(error)return <p>{error}</p>
+  if(loading)return <p>Carregando...</p>
+  if(data)
+    return (    
+      <div>
+
+    {data.map((produto)=> (<h1 key={produto.id}>{produto.nome}</h1> ))}
+      </div>
+    );
+  else return null;
 }
 
 
--- src/useLocalStorage.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-const useLocalStorage = (key, inicial) =>{
-    const [state, setState] = React.useState
-    const local = window.localStorage.getItem(key)
-    return local ? local : inicial
-}
