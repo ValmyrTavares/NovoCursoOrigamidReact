aee022095e9dc7d53ae8855d51bffebf36939f47
Valmyr Tavares <valmyr.lima@millennium.com.br>
2020-07-23 10:06:40 -0300

    Esse é um bélissimo exemplo de form, claro que bem simples o básico mas já tratando o state como um objeto e desconstruindo o objeto event e tal, como é sempre feito. Esse é um pré para o próximo que será um form com post e mais próximo da realidade

--- src/App.js
+++ src/App.js
@@ -1,27 +1,38 @@
 import React from 'react'
-import useFetch from './useFetch'
-
 
 
 const App = ()=>{
+  const [form, setForm] = React.useState({
+    nome: '',
+    email:'',
+    idade: undefined
+  })
 
-  const {request, data, loading, error} = useFetch();
-
-  React.useEffect(()=> {
-    request('https://ranekapi.origamid.dev/json/api/produto')
-  }, [])
-
-
-  if(error)return <p>{error}</p>
-  if(loading)return <p>Carregando...</p>
-  if(data)
+  function handleChange({target}){
+    const {id, value} = target
+   setForm({...form, [id]: value})
+    console.log(id, value)
+  }
+  
     return (    
       <div>
+        <form >
+          <label htmlFor="nome">Nome</label>
+          <input id="nome" name="nome" type='text' value={form.nome} onChange={handleChange}/>
+          <label htmlFor="email">Email</label>
+          <input id="email" name="email" type='text' value={form.email} onChange={handleChange}/>
+          <label htmlFor="idade">Idade</label>
+          <input id="idade" name="idade" type='number' value={form.idade} onChange={handleChange}/>
+          <p>{form.email}</p>
+          <p>{form.nome}</p>
+          <p>{form.idade}</p>
+          <button>Enviar</button>
+        </form>
 
-    {data.map((produto)=> (<h1 key={produto.id}>{produto.nome}</h1> ))}
+
       </div>
     );
-  else return null;
+ 
 }
 
 
