2fec031e2d4f75ac906274bacb4b82b1366aeb32
Valmyr Tavares <valmyr.lima@millennium.com.br>
2020-07-24 18:16:45 -0300

    Aula 409 Falando de Radio,

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React from "react";
 import Input from "./Forms/Input";
 import Select from "./Forms/Select";
+import Radio from "./Forms/Radio";
 
 const App = () => {
 const [nome, setNome] = React.useState("")
 const [email, setEmail] = React.useState("")
 const [produto, setProduto]= React.useState('');
+const [cor, setCor] = React.useState('')
 
 
   return (
   <div>
+    <Radio  options={["Azul","Vermelho"]} value={cor} setValue={setCor}/>
     <Select options={["Paris", "Firenzi", "Roma","Buenos Aires","Mendonza","Verona"]} value={produto} setValue={setProduto}/>
     <Input id="nome" label="Nome"/>
     <Input id="email" label="Email"/>
