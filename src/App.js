7e8c306be1076361afa352b3e84ab617b8197441
Valmyr Tavares <valmyr.lima@millennium.com.br>
2020-07-23 22:21:17 -0300

    Nessa aula eu instaeli o desenvolvedor do react dentro do Chome, não é nada intuitivo mas eu consegui usar. A questão aqui é que o setSTATE INCLUI  tanto quando vc tira quanto quando vc inclui algo. Então existe uma série de procedimentos para evitar issl. Claro que parece um tanatto de preciosismo, mas é na dificuldade muitas vezes que se ganha músculos

--- src/App.js
+++ src/App.js
@@ -6,17 +6,34 @@ import React from 'react'
 const App = ()=>{
   
 
-  const [termos, setTermos]= React.useState(false)   
+  const [cores, setCores]= React.useState(['vermelho'])   
+
+  function handleChange({target}){
+    if(target.checked){
+      setCores([...cores, target.value])
+      }else{
+        setCores(cores.filter((cor)=> cor !==target.value))
+      }
+  }
+
+   
+    console.log(cores)
+  
+
   
     return (    
       <div>
         <form  >
           <label>
-            <input value={termos} type="checkbox" checked={termos} onChange={({target}) => setTermos(target.checked)}/>
-            Aceito os termos
+            <input value="azul" checked={cores.includes("azul")} type="checkbox"  onChange={handleChange}/>
+           Azul
+          </label>
+          <label>
+            <input value="vermelho" checked={cores.includes("vermelho")}type="checkbox"  onChange={handleChange}/>
+           Vermelho
           </label>
         </form>
-        {termos?"Está feito":"Vc precisa aceitar"}
+      
      
       </div>
 );
